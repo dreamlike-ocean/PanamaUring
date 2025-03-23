624688085d6e24ab082cbb60fa8d1ef8899d3184
dreamlike-ocean <dreamlike.vertx@gmail.com>
2025-03-23 20:42:24 +0800

    1,支持macos的测试构建

--- panama-generator/src/test/rust/build.sh
+++ panama-generator/src/test/rust/build.sh
@@ -1,4 +1,16 @@
 rm -f ../resources/libperson.so
-cargo build
+cargo build || { echo "cargo build fail!"; exit 1; }
 mkdir -p ../resources
-mv target/debug/libperson.so ../resources/libperson.so
+
+platform=$(uname -s)
+
+if [[ "$platform" == "Linux" ]]; then
+  mv target/debug/libperson.so ../resources/libperson.so
+  echo "build on Linux"
+elif [[ "$platform" == "Darwin" ]]; then
+  mv target/debug/libperson.dylib ../resources/libperson.so
+  echo "build on MacOS"
+else
+  echo "Unsupported platform"
+  exit 1
+fi
